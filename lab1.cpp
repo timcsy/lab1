7d2d3e8b5d7909005d4ae82c616dbcaead5472e8
timcsy <timocsy@yahoo.com.tw>
2016-03-14 20:25:56 +0800

    Adding Problem B files

---
+++ lab1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	while(n!=1)
+	{
+		if(n%2 == 1) n = 3*n+1;
+		else n = n/2;
+		cout<<n<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
